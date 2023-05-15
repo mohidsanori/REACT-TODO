cb587f108087017c6f6313548e4bf425c5caadd3
Mohid <mohidahmed555@gmail.com>
2023-05-15 17:56:23 +0500

    Sorting with Object Array

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../images/logo.png';
 import './todo.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,25 +9,37 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 const Todo = () => {
   const [data, setData] = useState('');
   const [todo, setTodo] = useState([]);
-  const [order, setOrder] = useState([]);
+
+  useEffect(() => {
+    const storedTodo = localStorage.getItem('todo');
+
+    if (storedTodo) {
+      setTodo(JSON.parse(storedTodo));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todo', JSON.stringify(todo));
+  }, [todo]);
 
   const addTodo = () => {
     if (!data) {
       return;
     }
 
-    setTodo([...todo, data]);
-    setOrder([...order, todo.length]);
+    const newTodo = { text: data, sort: todo.length };
+    setTodo([...todo, newTodo]);
     setData('');
   };
 
-  const deleteTodo = (id) => {
-    const updatedTodo = todo.filter((element, index) => index !== id);
-    const updatedOrder = order.filter((index) => index !== id).map((index) => {
-      return index > id ? index - 1 : index;
-    });
-    setTodo(updatedTodo);
-    setOrder(updatedOrder);
+   const deleteTodo = (id) => {
+    const updatedTodo = todo.filter((_, index) => index !== id);
+    setTodo(
+      updatedTodo.map((item, index) => ({
+        ...item,
+        sort: index,
+      }))
+    );
   };
 
   const dragStart = (e, id) => {
@@ -40,22 +52,19 @@ const Todo = () => {
 
   const drop = (e, id) => {
     const dragId = e.dataTransfer.getData('text/plain');
-    const dragIndex = order.findIndex((index) => index === Number(dragId));
-    const dropIndex = order.findIndex((index) => index === id);
+    const dragIndex = todo.findIndex((_, index) => index === Number(dragId));
+    const dropIndex = todo.findIndex((_, index) => index === id);
 
-    const updatedOrder = [...order];
-    updatedOrder.splice(dragIndex, 1);
-    updatedOrder.splice(dropIndex, 0, Number(dragId));
+    const updatedTodo = [...todo];
+    const [dragItem] = updatedTodo.splice(dragIndex, 1);
+    updatedTodo.splice(dropIndex, 0, dragItem);
 
-    setOrder(updatedOrder);
-  };
-
-  const dragTodo = (id) => {
-    const dragIndex = order.findIndex((index) => index === id);
-    const updatedOrder = [...order];
-    updatedOrder.splice(dragIndex, 1);
-    updatedOrder.push(id);
-    setOrder(updatedOrder);
+    setTodo(
+      updatedTodo.map((item, index) => ({
+        ...item,
+        sort: index,
+      }))
+    );
   };
 
   return (
@@ -79,33 +88,29 @@ const Todo = () => {
         </div>
 
         <div className="showItems">
-          {order.map((index) => {
-            const element = todo[index];
-            return (
-              <div
-                className="each"
-                key={index}
-                draggable="true"
-                onDragStart={(e) => dragStart(e, index)}
-                onDragOver={dragOver}
-                onDrop={(e) => drop(e, index)}
-              >
-                <FontAwesomeIcon
-                  icon={faBars}
-                  className="add-btn drag-btn"
-                  title="Drag Item"
-                  onMouseDown={() => dragTodo(index)}
-                />
-                <h3>{element}</h3>
-                <FontAwesomeIcon
-                  icon={faTrashAlt}
-                  className="add-btn"
-                  title="Delete Item"
-                  onClick={() => deleteTodo(index)}
-                />
-              </div>
-            );
-          })}
+          {todo.map((todoItem, index) => (
+            <div
+              className="each"
+              key={index}
+              draggable="true"
+              onDragStart={(e) => dragStart(e, index)}
+              onDragOver={dragOver}
+              onDrop={(e) => drop(e, index)}
+            >
+              <FontAwesomeIcon
+                icon={faBars}
+                className="add-btn drag-btn"
+                title="Drag Item"
+              />
+              <h3>{todoItem.text}</h3>
+              <FontAwesomeIcon
+                icon={faTrashAlt}
+                className="add-btn"
+                title="Delete Item"
+                onClick={() => deleteTodo(index)}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </>
